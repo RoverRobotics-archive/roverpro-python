9a220d4b81d5bd87be3f9c1b7cae86f4c74bc64a
Dan Rose <dan@digilabs.io>
2018-11-20 17:55:36 -0600

    Add convenience function to find ports More work on  encoder feedback

--- openrover/openrover.py
+++ openrover/openrover.py
@@ -1,8 +1,13 @@
 from time import sleep
-from typing import Callable
+from typing import Callable, Iterable, List
+
+from serial import Serial
 import serial.threaded
+from serial.tools import list_ports
 
 from .openrover_data import OPENROVER_DATA_ELEMENTS
+
+DEFAULT_SERIAL_KWARGS = dict(baudrate=57600, timeout=0.5, write_timeout=0.5, stopbits=1)
 
 
 class OpenRoverProtocol(serial.threaded.Protocol):
@@ -10,8 +15,8 @@ class OpenRoverProtocol(serial.threaded.Protocol):
     on_data_read: Callable[[str, int], None] = None
     transport = None  #
 
-    @staticmethod
-    def checksum(values):
+    @classmethod
+    def checksum(cls, values):
         return 255 - sum(values) % 255
 
     def __init__(self):
@@ -46,24 +51,28 @@ class OpenRoverProtocol(serial.threaded.Protocol):
             if packet[4] == self.checksum(packet[1:4]):
                 k = packet[1]
                 v = OPENROVER_DATA_ELEMENTS[k].data_format.unpack(packet[2:4])
+
                 on_data_read = self.on_data_read
                 if on_data_read is not None:
                     on_data_read(k, v)
             else:
                 pass  # packet is bad. ignore it
 
-    @staticmethod
-    def encode_speed(speed_as_float):
-        return int(round(speed_as_float * 125) + 125)
+    @classmethod
+    def encode_speed(cls, speed_as_float):
+        return int(round(speed_as_float * 125)) + 125
 
-    def write(self, motor_left, motor_right, flipper, arg1, arg2):
-        payload = [self.encode_speed(motor_left),
-                   self.encode_speed(motor_right),
-                   self.encode_speed(flipper),
+    @classmethod
+    def encode_packet(cls, motor_left, motor_right, flipper, arg1, arg2):
+        payload = [cls.encode_speed(motor_left),
+                   cls.encode_speed(motor_right),
+                   cls.encode_speed(flipper),
                    arg1,
                    arg2]
-        packet = bytearray([self.SERIAL_START_BYTE] + payload + [self.checksum(payload)])
-        return self.transport.write(packet)
+        return bytes([cls.SERIAL_START_BYTE] + payload + [cls.checksum(payload)])
+
+    def write(self, *args):
+        return self.transport.write(self.encode_packet(*args))
 
 
 class OpenRover:
@@ -81,10 +90,15 @@ class OpenRover:
         self._motor_flipper = 0
         self._latest_data = dict()
 
-    def open(self, port, **serial_kwargs):
-        kwargs = dict(baudrate=57600, timeout=0.5, write_timeout=0.5, stopbits=1)
+    def open(self, port=None, **serial_kwargs):
+        if port is None:
+            port = list_openrover_devices()[0]
+
+        kwargs = DEFAULT_SERIAL_KWARGS.copy()
         kwargs.update(serial_kwargs)
         serial_device = serial.Serial(port, **kwargs)
+        if not get_openrover_version(serial_device):
+            raise ValueError(f'Device {port} did not respond to a request for OpenRover version number. Is it an OpenRover? Is it powered on?')
         self._reader_thread = serial.threaded.ReaderThread(serial_device, OpenRoverProtocol)
         self._reader_thread.start()
         self._reader_thread.connect()
@@ -106,9 +120,6 @@ class OpenRover:
         self._motor_flipper = flipper
 
     def send_command(self, arg1, arg2):
-        assert 0 <= arg1 <= 255
-        assert 0 <= arg2 <= 255
-
         self._protocol.write(self._motor_left, self._motor_right, self._motor_flipper, arg1, arg2)
 
     def send_speed(self):
@@ -130,3 +141,55 @@ class OpenRover:
 
     def get_data(self, index):
         return self._latest_data.get(index)
+
+
+class OpenRoverException(Exception):
+    pass
+
+
+def get_openrover_version(s: Serial):
+    """Checks if the given device is an OpenRover device.
+    If it is, returns the build number.
+    Otherwise raises an OpenRoverException"""
+    _ = s.readall()
+    s.write(bytes([253, 125, 125, 125, 10, 40, 85]))
+    test_value = bytes([253, 40])
+    device_output = s.read_until(test_value)
+    if device_output.endswith(test_value):
+        version_bytes = s.read(3)
+        return (version_bytes[0] << 8) + version_bytes[1]
+    else:
+        raise OpenRoverException
+
+
+def iterate_openrovers():
+    """
+    Returns a list of devices determined to be OpenRover devices.
+    Throws a SerialException if candidate devices are busy
+    """
+    for c in list_ports.comports():
+        is_openrover = False
+        port = c.device
+
+        if c.manufacturer == 'FTDI':
+            s = Serial(port, **DEFAULT_SERIAL_KWARGS)
+            try:
+                get_openrover_version(s)
+                is_openrover = True
+            except OpenRoverException:
+                pass
+            finally:
+                s.close()
+
+        if is_openrover:
+            yield port
+
+
+def find_openrover():
+    """
+    Find the first OpenRover device and return its port
+    """
+    try:
+        return next(iterate_openrovers())
+    except StopIteration:
+        raise OpenRoverException
