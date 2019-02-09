d40c1ca4692991df98068eff5e89cdc5ab3f5aa2
Dan Rose <dan@digilabs.io>
2019-02-08 18:09:19 -0600

    FIX TEST LAUNCHER FOR REAL

--- openrover/test.py
+++ openrover/test.py
@@ -1,11 +1,8 @@
-from importlib.util import find_spec
-
 import pytest
 
 
 def main():
-    openrover_search_paths = find_spec('openrover').submodule_search_locations
-    return pytest.main(openrover_search_paths)
+    return pytest.main(['--pyargs', 'openrover'])
 
 
 if __name__ == '__main__':
